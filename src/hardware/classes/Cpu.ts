778857f65b592f0d251c02b9672be0a0fd0886b8
Nissoco <nexabra@outlook.com>
2023-12-08 15:06:04 -0600

    Added patches to `CPU.ts`

--- src/hardware/classes/Cpu.ts
+++ src/hardware/classes/Cpu.ts
@@ -1,21 +1,26 @@
-export default class Cpu {
+class CPU {
     public readonly id: number;
     public readonly model: string;
-    public readonly brand: string;
+    public readonly vendor: string;
     public readonly tdp: number;
     public readonly architecture: string;
     public readonly graphics: string | null;
-    public readonly cores: Core[];
+    public readonly cores: CPU.Core[];
+    // CPU Cache in kilobytes
+    public readonly cache: {l1: number, l2: number, l3: number | null};
+    public readonly releaseDate: Date | null
 
 
-    constructor(id: number, model: string, brand: string, tdp: number, architecture: string, graphics: string | null, cores: Core[]) {
+    constructor(id: number, model: string, vendor: string, tdp: number, architecture: string, graphics: string | null, cores: CPU.Core[], releaseDate: Date | null, cache: {l1: number, l2: number, l3: number | null}) {
         this.id = id;
         this.model = model;
-        this.brand = brand;
+        this.vendor = vendor;
         this.tdp = tdp;
         this.architecture = architecture;
         this.graphics = graphics;
         this.cores = cores;
+        this.releaseDate = releaseDate
+        this.cache = cache
     }
 
     public get totalCoreCount() {
@@ -27,17 +32,21 @@ export default class Cpu {
     }
 }
 
-export class Core {
-    public readonly level: number;
-    public readonly frequency: number;
-    public readonly turboFrequency: number;
-    public readonly threadsPerCore: number;
-    public readonly nCores: number;
-    constructor(level: number, frequency: number, turboFrequency: number, threadsPerCore: number, nCores: number) {
-        this.level = level;
-        this.frequency = frequency;
-        this.threadsPerCore = threadsPerCore;
-        this.turboFrequency = turboFrequency;
-        this.nCores = nCores;
+namespace CPU {
+    export class Core {
+        public readonly level: number;
+        public readonly frequency: number;
+        public readonly boostFrequency: number;
+        public readonly threadsPerCore: number;
+        public readonly nCores: number;
+        constructor(level: number, frequency: number, boostFrequency: number, threadsPerCore: number, nCores: number) {
+            this.level = level;
+            this.frequency = frequency;
+            this.threadsPerCore = threadsPerCore;
+            this.boostFrequency = boostFrequency;
+            this.nCores = nCores;
+        }
     }
 }
+
+export default CPU;
